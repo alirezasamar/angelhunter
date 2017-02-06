38e3419f84c99d9fa5205fbe9fe837ae2ce9900e
ihar <westdvina@gmail.com>
2017-02-06 23:22:08 +0300

    fix databases

--- angel/settings.py
+++ angel/settings.py
@@ -86,8 +86,7 @@ WSGI_APPLICATION = 'angel.wsgi.application'
 #     }
 # }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES = {'default': dj_database_url.config(conn_max_age=500)}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
