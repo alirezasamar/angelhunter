ecb27b9a73f4945c43dd2aab45e0fd60b9e3c0f6
ihar <westdvina@gmail.com>
2017-02-06 21:16:45 +0300

    add settings staticfiles

--- angel/settings.py
+++ angel/settings.py
@@ -138,6 +138,14 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
+ADMIN_MEDIA_PREFIX = ‘/static/admin/’
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
 
 # LOGIN_URL = '/'
 # LOGIN_REDIRECT_URL = '/dashboard'
